25a42e4783705448ed5f8453e614bc0b5fe01822
ysjeung <yunsukjeung@snu.ac.kr>
2025-02-26 01:27:12 +0900

    starting for mono ulp

--- ergogen/footprints/nicenano.js
+++ ergogen/footprints/nicenano.js
@@ -5,57 +5,57 @@
 //    if up, power led will face away from pcb
 
 module.exports = {
-    params: {
-        designator: 'MCU',
-        orientation: 'down',
-        RAW: { type: 'net', value: 'RAW' },
-        GND: { type: 'net', value: 'GND' },
-        RST: { type: 'net', value: 'RST' },
-        VCC: { type: 'net', value: 'VCC' },
-        P031: { type: 'net', value: 'P031' },
-        P029: { type: 'net', value: 'P029' },
-        P002: { type: 'net', value: 'P002' },
-        P115: { type: 'net', value: 'P115' },
-        P113: { type: 'net', value: 'P113' },
-        P111: { type: 'net', value: 'P111' },
-        P010: { type: 'net', value: 'P010' },
-        P009: { type: 'net', value: 'P009' },
-        P006: { type: 'net', value: 'P006' },
-        P008: { type: 'net', value: 'P008' },
-        P017: { type: 'net', value: 'P017' },
-        P020: { type: 'net', value: 'P020' },
-        P022: { type: 'net', value: 'P022' },
-        P024: { type: 'net', value: 'P024' },
-        P100: { type: 'net', value: 'P100' },
-        P011: { type: 'net', value: 'P011' },
-        P104: { type: 'net', value: 'P104' },
-        P106: { type: 'net', value: 'P106' },
-        P101: { type: 'net', value: 'P101' },
-        P102: { type: 'net', value: 'P102' },
-        P107: { type: 'net', value: 'P107' }
-    },
-    body: p => {
-        const standard = `
+  params: {
+    designator: "MCU",
+    orientation: "down",
+    RAW: { type: "net", value: "RAW" },
+    GND: { type: "net", value: "GND" },
+    RST: { type: "net", value: "RST" },
+    VCC: { type: "net", value: "VCC" },
+    P031: { type: "net", value: "D21" },
+    P029: { type: "net", value: "D20" },
+    P002: { type: "net", value: "D19" },
+    P115: { type: "net", value: "D18" },
+    P113: { type: "net", value: "D15" },
+    P111: { type: "net", value: "D14" },
+    P010: { type: "net", value: "D16" },
+    P009: { type: "net", value: "D10" },
+    P006: { type: "net", value: "D01" },
+    P008: { type: "net", value: "D00" },
+    P017: { type: "net", value: "D02" },
+    P020: { type: "net", value: "D03" },
+    P022: { type: "net", value: "D04" },
+    P024: { type: "net", value: "D05" },
+    P100: { type: "net", value: "D06" },
+    P011: { type: "net", value: "D07" },
+    P104: { type: "net", value: "D08" },
+    P106: { type: "net", value: "D09" },
+    P101: { type: "net", value: "P101" },
+    P102: { type: "net", value: "P102" },
+    P107: { type: "net", value: "P107" },
+  },
+  body: (p) => {
+    const standard = `
         (module nice_nano (layer F.Cu) (tedit 6058B206)
         ${p.at /* parametric position */}
 
-        ${'' /* footprint reference */}
+        ${"" /* footprint reference */}
         (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.2 1.2) (thickness 0.2032))))
 
-        ${''/* illustration of the (possible) USB port overhang */}
+        ${"" /* illustration of the (possible) USB port overhang */}
         (fp_line (start -14.224 -3.556) (end -14.224 3.81) (layer Dwgs.User) (width 0.2))
         (fp_line (start -14.224 3.81) (end -19.304 3.81) (layer Dwgs.User) (width 0.2))
         (fp_line (start -19.304 3.81) (end -19.304 -3.556) (layer Dwgs.User) (width 0.2))
         (fp_line (start -19.304 -3.556) (end -14.224 -3.556) (layer Dwgs.User) (width 0.2))
 
-        ${''/* component outline */}
+        ${"" /* component outline */}
         (fp_line (start 15.24 -8.89) (end -17.78 -8.89) (layer F.SilkS) (width 0.381))
         (fp_line (start -17.78 8.89) (end 15.24 8.89) (layer F.SilkS) (width 0.381))
         (fp_line (start -17.78 -8.89) (end -17.78 8.89) (layer F.SilkS) (width 0.381))
-        `
-        function pins(def_neg, def_pos) {
-            return `
-          ${''/* pin names */}
+        `;
+    function pins(def_neg, def_pos) {
+      return `
+          ${"" /* pin names */}
           (fp_text user RAW (at -13.97 ${def_pos}5.473715 ${p.rot + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
           (fp_text user GND (at -11.43 ${def_pos}5.454667 ${p.rot + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
           (fp_text user RST (at -8.89 ${def_pos}5.588 ${p.rot + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
@@ -79,7 +79,7 @@ module.exports = {
           (fp_text user 100 (at 6.35 ${def_neg}5.53719 ${p.rot + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
           (fp_text user 104 (at 11.43 ${def_neg}5.53719 ${p.rot + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
 
-          ${''/* and now the actual pins */}
+          ${"" /* and now the actual pins */}
           (pad 1 smd circle (at -13.97 ${def_neg}7.62) (size 1.7526 1.7526) (drill 1.0922) (layers F.Cu F.SilkS F.Mask) ${p.P006.str})
           (pad 2 smd circle (at -11.43 ${def_neg}7.62) (size 1.7526 1.7526) (drill 1.0922) (layers F.Cu F.SilkS F.Mask) ${p.P008.str})
           (pad 3 smd circle (at -8.89 ${def_neg}7.62) (size 1.7526 1.7526) (drill 1.0922) (layers F.Cu F.SilkS F.Mask) ${p.GND.str})
@@ -105,18 +105,18 @@ module.exports = {
           (pad 22 smd circle (at -8.89 ${def_pos}7.62) (size 1.7526 1.7526) (drill 1.0922) (layers F.Cu F.SilkS F.Mask) ${p.RST.str})
           (pad 23 smd circle (at -11.43 ${def_pos}7.62) (size 1.7526 1.7526) (drill 1.0922) (layers F.Cu F.SilkS F.Mask) ${p.GND.str})
           (pad 24 smd circle (at -13.97 ${def_pos}7.62) (size 1.7526 1.7526) (drill 1.0922) (layers F.Cu F.SilkS F.Mask) ${p.RAW.str})
-          `
-        }
-        if (p.orientation == 'down') {
-            return `
-            ${standard}
-            ${pins('-', '')})
-            `
-        } else {
-            return `
-            ${standard}
-            ${pins('', '-')})
-            `
-        }
+          `;
     }
-}
+    if (p.orientation == "down") {
+      return `
+            ${standard}
+            ${pins("-", "")})
+            `;
+    } else {
+      return `
+            ${standard}
+            ${pins("", "-")})
+            `;
+    }
+  },
+};
